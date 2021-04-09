fa0a361a1e7312139029eafa9507d6f0e185b9d5
Wairimu-Miano <wairimu.miano@student.moringaschool.com>
2021-04-09 13:47:03 +0300

    Add function to return name after checking gender

--- js/scripts.js
+++ js/scripts.js
@@ -22,4 +22,14 @@ function newDate=new Date(dob){
   var yearYY=parseInt(zellerYear.substr(2,2))
   //Insert FORMULA
   var day=((parseInt((26*(zellerMonth+1)/10))+ zellerYear+parseInt(zellerYear/4)-parseInt(zellerYear/100)+parseInt(zellerYear/400)+date)%7)
-    
+  //FUNCTION TO RETURN Name, after checking gender
+  function name(day){
+    const form =document.forms.gender-form
+    const radios=form.elements.gender
+    const value=Array.from(radios).find(radio=>radio.checked).value
+    if value==="male"{
+      alert("Your Akan name is " + maleNames[day])
+    } else{
+      alert("Your Akan name is " + femaleNames[day])
+    }
+  }
