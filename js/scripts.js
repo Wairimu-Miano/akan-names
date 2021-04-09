a55b1f2e0ef565ba04375f51f70abc41cd70ac3d
Wairimu-Miano <wairimu.miano@student.moringaschool.com>
2021-04-09 13:14:04 +0300

    Create a function to extract date, month and year from entered dates of Birth

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 //Arrays to store Akan names
 var maleNames=['Kwame','Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi']
 var femaleNames=['Ama','Akosua','Adwoa','Abenaa','Akua','Yaa','Afua']
+//Create a function to extract date, month and year from entered dates of Birth
+function newDate=new Date(dob){
+  var month=newDate.getMonth()+1
+  var date=newDate.getDate()
+  var year=newDate.getFullYear()
+  console.log(month+"-"+date+"-"+year)
+}
