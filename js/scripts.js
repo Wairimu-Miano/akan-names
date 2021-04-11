711f75394aace6632792c68685a31f34ad40182a
Wairimu-Miano <wairimu.miano@student.moringaschool.com>
2021-04-11 13:51:29 +0300

    Add a function to extract dates from form input

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,14 @@
 var maleNames=['Kwame','Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi']
 var femaleNames=['Ama','Akosua','Adwoa','Abenaa','Akua','Yaa','Afua']
 //Create a function to extract date, month and year from entered dates of Birth
-function newDate=new Date(dob){
-  var month=newDate.getMonth()+1
-  var date=newDate.getDate()
-  var year=newDate.getFullYear()
-  console.log(month+"-"+date+"-"+year)
-}
+(function() {
+    $('#submit').on('click', function(){
+      var dateInput = new Date($('#dob').val());
+      date = dateInput.getDate();
+      month = dateInput.getMonth() + 1;
+      year = dateInput.getFullYear();
+    });
+})();
 //CALCULATE DAY OF THE WEEK USING FORMULA
   //We first need to adjust the months to fit working using Zeller's Algorithm
   if (month===1||month===2){
