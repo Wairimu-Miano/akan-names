7fbec7936253c63ea0eb0aebf0d06b299453e01f
Wairimu-Miano <wairimu.miano@student.moringaschool.com>
2021-04-11 19:38:57 +0300

    Adjust the month and year to fit zeller's algorithm

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,16 @@ function submitStaff(){
   var date = dateInput.getDate();
   var month = dateInput.getMonth() + 1;
   var year = dateInput.getFullYear();
+  //CALCULATE DAY OF THE WEEK USING FORMULA
+  //We first need to adjust the months to fit working using Zeller's Algorithm:For January month=13,Feb month=14, others are normal
+  if (month===1||month===2){
+    zellerMonth=month+12
+    zellerYear=year-1
+  } else {
+    zellerMonth=month
+    zellerYear=year
+  }
+  //Then we divide the year into two, century part and actual Year of the century
+  var century=parseInt(zellerYear.toString().substr(0,2))
+  var yearYY=parseInt(zellerYear.toString().substr(2,2))
 }
