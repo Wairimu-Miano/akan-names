eeecbe9533ea5c9cae1f419f2fa623b3617f9899
Wairimu-Miano <wairimu.miano@student.moringaschool.com>
2021-04-11 19:36:44 +0300

    Create variables to store date of birth info collected:month,date,year

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 //Arrays to store Akan names
 var maleNames=['Kwame','Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi']
 var femaleNames=['Ama','Akosua','Adwoa','Abenaa','Akua','Yaa','Afua']
-//Create a function to extract date, month and year from entered dates of Birth
+//Create a function
+//Create a variable to store the extracted date entered in the form
+function submitStaff(){
+  //Create variables to store the extracted date entered in the form
+  var dateInput=new Date(form.dob.value)
+  //Extract month, year and date
+  var date = dateInput.getDate();
+  var month = dateInput.getMonth() + 1;
+  var year = dateInput.getFullYear();
+}
